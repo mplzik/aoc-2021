2cb3aa992dad2a743fa5ef97d4bc7a63abe8a8fd
Milan Plzik <milan.plzik@gmail.com>
2021-12-13 07:26:30 +0100

    Day 13 solutions.
    Signed-off-by: Milan Plzik <milan.plzik@gmail.com>

---
+++ day13b/src/main.rs
@@ -0,0 +1,58 @@
+type Point = [usize; 2];
+use std::collections::HashSet;
+
+fn fold(page: &HashSet<Point>, axis: &str, pos: usize) -> HashSet<Point> {
+    let mut ret: HashSet<Point> = HashSet::new();
+
+    for point in page.iter().cloned() {
+        let newpoint = match axis {
+            "x" if point[0] > pos => [(2 * pos as isize - point[0] as isize) as usize, point[1]],
+            "y" if point[1] > pos => [point[0], (2 * pos as isize - point[1] as isize) as usize],
+            _ => point,
+        };
+        ret.insert(newpoint);
+    }
+    ret
+}
+
+fn print(p: &HashSet<Point>) {
+    let maxx = p.iter().map(|x| x[0]).max().unwrap();
+    let maxy = p.iter().map(|x| x[1]).max().unwrap();
+    for y in 0..=maxy {
+        for x in 0..=maxx {
+            if p.contains(&[x, y]) {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!("");
+    }
+}
+
+use std::io::{self, BufRead};
+fn main() {
+    let stdin = io::stdin();
+    let lines = &mut stdin.lock().lines();
+    let mut points: HashSet<Point> = HashSet::new();
+
+    for line in lines.map(|x| x.unwrap()).take_while(|x| x != "") {
+        let point = <Point>::try_from(
+            line.split(',')
+                .map(|x| x.parse::<usize>().unwrap())
+                .collect::<Vec<usize>>(),
+        )
+        .unwrap();
+        points.insert(point);
+    }
+
+    for line in lines.map(|x| x.unwrap()) {
+        assert!(line.starts_with("fold along "));
+
+        let from = "fold along ".len();
+        let fold_ins = &line[from..].split('=').collect::<Vec<&str>>();
+        points = fold(&points, fold_ins[0], fold_ins[1].parse::<usize>().unwrap());
+    }
+
+    print(&points);
+}
